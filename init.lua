1306d8eeca088e1e6aefcef9b82b07033ea84109
Thomas Bonk <thomas@bonk.dev>
2021-02-02 14:30:16 +0100

    Send different keys for brave

--- init.lua
+++ init.lua
@@ -16,9 +16,9 @@ local flagsChangedListeners = {}
 
 function createObserver (eventTypes, listeners)
    return hs.eventtap.new(eventTypes, function(event)
-        local preventDefault = false
-        each(function (handler) if handler(event) then preventDefault = preventDefault or true end end, listeners)
-        return preventDefault
+      local preventDefault = false
+      each(function (handler) if handler(event) then preventDefault = preventDefault or true end end, listeners)
+      return preventDefault
    end)
 end
 
@@ -97,18 +97,18 @@ function addStandaloneHandler(modifierCode, modifier, standaloneHandler)
    local modifierUsed = false
 
    table.insert(flagsChangedListeners, function(event)
-         local flags = event:getFlags()
-         local keyCode = event:getKeyCode()
-         if keyCode == modifierCode then
-            if not flags[modifier] and not modifierUsed then
-               standaloneHandler()
-            end
-            modifierUsed = false
+      local flags = event:getFlags()
+      local keyCode = event:getKeyCode()
+      if keyCode == modifierCode then
+         if not flags[modifier] and not modifierUsed then
+            standaloneHandler()
          end
+         modifierUsed = false
+      end
    end)
 
    table.insert(keyDownListeners, function(event)
-         modifierUsed = modifierUsed or event:getFlags()[modifier]
+      modifierUsed = modifierUsed or event:getFlags()[modifier]
    end)
 end
 
@@ -117,13 +117,13 @@ function addStandaloneModifier(modifierCode, modifier, standaloneCode)
       modifierCode,
       modifier,
       function () hs.eventtap.event.newKeyEvent({}, standaloneCode, true):post() end
-   )
+      )
 end
 
 function printNonRecursiveKey(key) 
-  stopObservers()
-  hs.eventtap.event.newKeyEvent({}, key, true):post()
-  startObservers()
+   stopObservers()
+   hs.eventtap.event.newKeyEvent({}, key, true):post()
+   startObservers()
 end
 
 function isKitty()
@@ -138,93 +138,153 @@ function isKitty()
     return false
 end
 
+function isApplicable()
+   local win = hs.window.focusedWindow()
+   if win then
+      local app = win:application()
+      if app then
+         -- return app:bundleID() == 'net.kovidgoyal.kitty'
+         return true
+      end
+   end
+   return false
+end
+
+function sendNextKey() 
+   local win = hs.window.focusedWindow()
+   if win then
+      local app = win:application()
+      if app then
+         if app:bundleID() == 'com.brave.Browser' then
+            stopObservers()
+            hs.eventtap.event.newKeyEvent({"shift", "alt"}, 'J', true):post()
+            startObservers()
+         else 
+            printNonRecursiveKey('f19')
+         end
+      end
+   end
+end
+
+function sendPreviousKey() 
+   local win = hs.window.focusedWindow()
+   if win then
+      local app = win:application()
+      if app then
+         if app:bundleID() == 'com.brave.Browser' then
+            stopObservers()
+            hs.eventtap.event.newKeyEvent({"shift", "alt"}, 'K', true):post()
+            startObservers()
+         else 
+            printNonRecursiveKey('f20')
+         end
+      end
+   end
+end
+
+function sendSelectionEndKey() 
+   local win = hs.window.focusedWindow()
+   if win then
+      local app = win:application()
+      if app then
+         if app:bundleID() == 'com.brave.Browser' then
+            stopObservers()
+            hs.eventtap.event.newKeyEvent({"shift", "alt"}, 'L', true):post()
+            startObservers()
+         else 
+            printNonRecursiveKey('f18')
+         end
+      end
+   end
+end
+
 function addVimQuickFileNavigation() 
-  local log = hs.logger.new('mymodule','debug')
-  local modifierPressed = false
-  local modifierUsed = false
+   local log = hs.logger.new('mymodule','debug')
+   local modifierPressed = false
+   local modifierUsed = false
 
-  function reset()
-    modifierPressed = false
-    modifierUsed = false
-  end
+   function reset()
+      modifierPressed = false
+      modifierUsed = false
+   end
 
-  table.insert(keyDownListeners, function(event)
-    if not isKitty() then
-      return false
-    end
-
-    local keyCode = event:getKeyCode()
-    if keyCode == 3 and next(event:getFlags()) == nil then 
-      modifierPressed = true
-      return true
-    end
-    if modifierPressed then
-      if keyCode == 38 then 
-        printNonRecursiveKey('f19')
-        modifierUsed = true
-        return true
+   table.insert(keyDownListeners, function(event)
+      if not isKitty() then
+         return false
       end
-      if keyCode == 40 then 
-        printNonRecursiveKey('f20')
-        modifierUsed = true
-        return true
-      end
-      if not modifierUsed then 
-        printNonRecursiveKey('f')
-      end 
-    end
-    reset()
-    return false 
-  end)
 
-  table.insert(keyUpListeners, function(event)
-    if not isKitty() then
-      reset()
-      return false
-    end
-    if event:getKeyCode() == 3 and next(event:getFlags()) == nil then
-      if modifierPressed and not modifierUsed then 
-        printNonRecursiveKey('f')
+      local keyCode = event:getKeyCode()
+      if keyCode == 3 and next(event:getFlags()) == nil then 
+         modifierPressed = true
+         return true
       end
-      if modifierPressed and modifierUsed then 
-        printNonRecursiveKey('f18')
+      if modifierPressed then
+         if keyCode == 38 then 
+            sendNextKey()
+            modifierUsed = true
+            return true
+         end
+         if keyCode == 40 then 
+            sendPreviousKey()
+            modifierUsed = true
+            return true
+         end
+         if not modifierUsed then 
+            printNonRecursiveKey('f')
+         end 
       end
       reset()
-    end
-    return false 
-  end)
+      return false 
+   end)
+
+   table.insert(keyUpListeners, function(event)
+      if not isKitty() then
+         reset()
+         return false
+      end
+      if event:getKeyCode() == 3 and next(event:getFlags()) == nil then
+         if modifierPressed and not modifierUsed then 
+            printNonRecursiveKey('f')
+         end
+         if modifierPressed and modifierUsed then 
+            sendSelectionEndKey()
+         end
+         reset()
+      end
+      return false 
+   end)
 end
 
 function createAlternativeKeys(hotkeyDefinitions)
-  function printKey(code)
-    return function () hs.eventtap.event.newKeyEvent({}, code, true):post() end
-  end
+   function printKey(code)
+      return function () hs.eventtap.event.newKeyEvent({}, code, true):post() end
+   end
 
-  hs.hotkey.bind({'alt'}, 'j', printKey('down'))
-  hs.hotkey.bind({'alt'}, 'k', printKey('up'))
-  hs.hotkey.bind({'alt'}, 'h', printKey('left'))
-  hs.hotkey.bind({'alt'}, 'l', printKey('right'))
+   hs.hotkey.bind({'alt'}, 'j', printKey('down'))
+   hs.hotkey.bind({'alt'}, 'k', printKey('up'))
+   hs.hotkey.bind({'alt'}, 'h', printKey('left'))
+   hs.hotkey.bind({'alt'}, 'l', printKey('right'))
 end
 
 function openApp(bundleID)
-  return function () hs.application.launchOrFocusByBundleID(bundleID) end
+   return function () hs.application.launchOrFocusByBundleID(bundleID) end
 end
 
 local currentPrimaryApplication = 1
 function togglePrimaryApplications ()
-  local win = hs.window.focusedWindow()
-  if win then
-    local app = win:application()
-    if app then
-      local focusedBundleID = app:bundleID()
-      for index, value in ipairs (primaryApplications) do
-        if value == focusedBundleID and index == currentPrimaryApplication then
-          currentPrimaryApplication = math.fmod(index, 2) + 1
-        end
+   local win = hs.window.focusedWindow()
+   if win then
+      local app = win:application()
+      if app then
+         local focusedBundleID = app:bundleID()
+         for index, value in ipairs (primaryApplications) do
+            if value == focusedBundleID and index == currentPrimaryApplication then
+               currentPrimaryApplication = math.fmod(index, 2) + 1
+            end
+         end
       end
-    end
-  end
-  hs.application.launchOrFocusByBundleID(primaryApplications[currentPrimaryApplication])
+   end
+   hs.application.launchOrFocusByBundleID(primaryApplications[currentPrimaryApplication])
 end
 
 keyDownEventObserver = createObserver({hs.eventtap.event.types.keyDown}, keyDownListeners)
@@ -232,8 +292,8 @@ keyUpEventObserver = createObserver({hs.eventtap.event.types.keyUp}, keyUpListen
 flagsChangedEventObserver = createObserver({hs.eventtap.event.types.flagsChanged}, flagsChangedListeners)
 
 hs.hotkey.bind({'alt'}, 'd', function ()
-  debug = not debug
-  hs.notify.show('Hammerspoon', 'debug toggled', '')
+   debug = not debug
+   hs.notify.show('Hammerspoon', 'debug toggled', '')
 end)
 
 -- addCustomModifier(49, "shift", "space")
@@ -247,11 +307,11 @@ addVimQuickFileNavigation()
 startObservers()
 
 local printUmlaut = function (umlaut)
-  return function ()
-    -- stopObservers()
-    hs.eventtap.keyStrokes(umlaut)
-    -- startObservers()
-  end
+   return function ()
+      -- stopObservers()
+      hs.eventtap.keyStrokes(umlaut)
+      -- startObservers()
+   end
 end
 hs.hotkey.bind({'ctrl'}, 'a', printUmlaut('ä'))
 hs.hotkey.bind({'ctrl', 'shift'}, 'a', printUmlaut('g'))
@@ -268,15 +328,15 @@ hs.hotkey.bind({'cmd'}, '2', openApp(primaryApplications[2]))
 
 -- Reload config when any lua file in config directory changes
 function reloadConfig(files)
-  doReload = false
-  for _,file in pairs(files) do
-    if file:sub(-4) == '.lua' then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
+   doReload = false
+   for _,file in pairs(files) do
+      if file:sub(-4) == '.lua' then
+         doReload = true
+      end
+   end
+   if doReload then
+      hs.reload()
+   end
 end
 local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/init.lua', reloadConfig):start()
 hs.notify.show('Hammerspoon', 'Configuration loaded', '')
